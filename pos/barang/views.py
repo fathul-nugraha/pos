8e0e8c52477bb80c237089b6b263a0a4631b46d4
fathul-nugraha <65307483+fathul-nugraha@users.noreply.github.com>
2026-09-04 19:17:18 +0700

    Update views.py
    mengubah halaman index dari app barang menjadi dashboard.html

--- pos/barang/views.py
+++ pos/barang/views.py
@@ -2,7 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-  return render(request, "barang/barang.html")
-  
-def masterBarang(request):
-  return render(request, "barang/masterbarang.html")
+  return render(request, "barang/dashboard.html")
